5f04251ce7a3e87f5c012d91eb452c23be72f33d
luweiblues <weilu@dhcp-10-202-54-101.tamulink.tamu.edu>
2014-07-31 15:10:56 -0500

    finished location-based and friendship-based queries

---
+++ ridesharing/RSPostOriginAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  RSPostOriginAnnotation.m
+//  ridesharing
+//
+//  Created by Wei Lu on 7/26/14.
+//  Copyright (c) 2014 WEI LU. All rights reserved.
+//
+
+#import "RSPostOriginAnnotation.h"
+
+@implementation RSPostOriginAnnotation
+
+@end
